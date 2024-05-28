4df77f24a2b1acf46d3812f348876ffd07f3b6e9
bumble.zhou <bumble.zhou@gmail.com>
2024-05-28 17:18:43 +0800

    add timer 3

---
+++ src/timer_3_binding_arguments_to_a_handler.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+using namespace std;
+
+void print(const boost::system::error_code& /*e*/, boost::asio::steady_timer* t, int* count) {
+    if(*count < 5) {
+        cout << "[print] *count:" << *count << endl;
+        ++(*count);
+        cout << "[print] ++(*count);" << endl;
+        cout << "[print] expiry() + 1" << endl;
+        t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+        cout << "[print] t->async_wait()" << endl;
+        boost::system::error_code ec(-2, boost::system::system_category());
+        t->async_wait(boost::bind(print, ec, t, count));
+    }
+}
+
+int main() {
+    boost::asio::io_context io;
+    int count = 0;
+    cout << "[main] count:" << count << std::endl;
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(1));
+
+    cout << "[main] t.async_wait(boost::bind(...));" << endl;
+    boost::system::error_code ec(-1, boost::system::system_category());
+    t.async_wait(boost::bind(print, ec, &t, &count));
+    
+    cout << "[main] io.run()" << endl;
+    io.run();
+    cout << "[main] count:" << count << std::endl;
+
+    return 0;
+}
